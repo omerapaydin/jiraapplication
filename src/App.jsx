ad7e3bad20d902a7b35f615272586480d9d1d700
Ömer Apaydın <omerapaydin90@gmail.com>
2025-04-14 23:37:14 +0300

    App

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import { useState } from "react";
 import "./App.css";
 import TaskCreate from "./components/TaskCreate";
 import TaskList from "./components/TaskList";
+import { useState } from "react";
 
 function App() {
   const [tasks, setTasks] = useState([]);
-
   const createTask = (title, taskDesc) => {
-    const createTask = [
+    const createdTasks = [
       ...tasks,
       {
         id: Math.round(Math.random() * 999999),
@@ -15,17 +14,35 @@ function App() {
         taskDesc,
       },
     ];
-    setTasks(createTask);
+    setTasks(createdTasks);
+  };
+
+  const deleteTaskById = (id) => {
+    const afterDeletingTasks = tasks.filter((task) => {
+      return task.id !== id;
+    });
+    setTasks(afterDeletingTasks);
+  };
+  const editTaskById = (id, updatedTitle, updatedTaskDesc) => {
+    const updatedTasks = tasks.map((task) => {
+      if (task.id === id) {
+        return { id, title: updatedTitle, taskDesc: updatedTaskDesc };
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
   };
 
   return (
-    <>
-      <div className="App">
-        <TaskCreate onCreate={createTask} />
-        <h1>Görevler</h1>
-        <TaskList tasks={tasks} />
-      </div>
-    </>
+    <div className="App">
+      <TaskCreate onCreate={createTask} />
+      <h1>Görevler</h1>
+      <TaskList
+        tasks={tasks}
+        onDelete={deleteTaskById}
+        onUpdate={editTaskById}
+      />
+    </div>
   );
 }
 
